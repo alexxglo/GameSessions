eafda5be8f4c15cf7b88893ec5a60193ca4ed1f5
Alexandru Glodeanu <galex.phoenix@gmail.com>
2021-08-31 21:23:20 +0300

    finished tests on game session controller

--- src/test/java/com/playtika/gamesessions/controllers/GameSessionControllerTests.java
+++ src/test/java/com/playtika/gamesessions/controllers/GameSessionControllerTests.java
@@ -1,11 +1,18 @@
 package com.playtika.gamesessions.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.playtika.gamesessions.dto.SessionDTO;
 import com.playtika.gamesessions.models.GameSession;
+import com.playtika.gamesessions.repositories.GameSessionRepository;
 import com.playtika.gamesessions.security.models.Role;
 import com.playtika.gamesessions.security.models.User;
+import com.playtika.gamesessions.services.FilterService;
 import com.playtika.gamesessions.services.GameSessionQueriesService;
 import com.playtika.gamesessions.services.GameSessionService;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,16 +22,18 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -38,6 +47,8 @@ public class GameSessionControllerTests {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
 
     private Authentication auth = mock(Authentication.class);
 
@@ -47,16 +58,22 @@ public class GameSessionControllerTests {
     @MockBean
     private GameSessionService gameSessionService;
 
+    @MockBean
+    private FilterService filterService;
+
     @Mock
     private GameSession mockGameSession;
 
     @Mock
     private Pageable pageable;
 
-    @MockBean
+    @Mock
+    private GameSessionRepository gameSessionRepository;
+
+    @Mock
     private GameSessionQueriesService queriesService;
 
-    public User configureUserMock() {
+    private User configureUserMock() {
         User mockedUser = mock(User.class);
         Role role;
         List<Role> roles = new ArrayList<>();
@@ -88,15 +105,19 @@ public class GameSessionControllerTests {
         return mockedGameSession;
     }
 
+    @Before
+    private SecurityContext configureSecurityContext() {
+        SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+        Mockito.when(securityContext.getAuthentication()).thenReturn(auth);
+        when(auth.isAuthenticated()).thenReturn(true);
+        when(auth.getName()).thenReturn("auth");
+        SecurityContextHolder.setContext(securityContext);
+        return securityContext;
+    }
+
     @Test
     @WithMockUser(roles = "ADMIN")
     public void startSessionTest() throws Exception {
-        SecurityContext securityContext = Mockito.mock(SecurityContext.class);
-        Mockito.when(securityContext.getAuthentication()).thenReturn(auth);
-        SecurityContextHolder.setContext(securityContext);
-
-        when(auth.isAuthenticated()).thenReturn(true);
-        when(auth.getName()).thenReturn("auth");
         when(mockGameSession.getName()).thenReturn("game");
         when(mockedUser.getUsername()).thenReturn("user");
         when(gameSessionService.startGame(mockGameSession.getName(), auth.getName())).thenReturn(mockGameSession);
@@ -113,8 +134,8 @@ public class GameSessionControllerTests {
         GameSession gameSession = configureGameSessionMock();
         List<GameSession> gameSessions = new ArrayList<>();
         gameSessions.add(gameSession);
-
-        when(queriesService.getAll(pageable).toList()).thenReturn(gameSessions);
+        Page<GameSession> gameSessionPage = new PageImpl(gameSessions);
+        when(queriesService.getAll(any())).thenReturn(gameSessionPage);
 
         mockMvc.perform(get("/api/session/","pageable"))
                 .andExpect(status().isOk())
@@ -122,4 +143,100 @@ public class GameSessionControllerTests {
     }
 
 
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void endSessionTest() throws Exception {
+        GameSession gameSession = new GameSession();
+        gameSession.setName("testSession");
+
+        when(gameSessionService.endGame(any())).thenReturn(gameSession);
+
+        mockMvc.perform(get("/api/session/end"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(gameSession.getName()));
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void getCurrentSessionDurationTest() throws Exception {
+        String responseString = "Some minutes";
+
+        when(gameSessionService.currentDuration(any())).thenReturn(responseString);
+
+        mockMvc.perform(get("/api/session/info"))
+                .andExpect(status().isOk())
+                .andExpect(result -> responseString.equals(result));
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void getTodayPlaytimeTest() throws Exception {
+        String responseString = "Playtime test minutes";
+
+        when(gameSessionService.getTodayPlaytime(any())).thenReturn(responseString);
+
+        mockMvc.perform(get("/api/session/today"))
+                .andExpect(status().isOk())
+                .andExpect(result -> responseString.equals(result));
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void getPlaytimeByDayTest() throws Exception {
+        String responseString = "Some minutes";
+
+        when(gameSessionService.getTotalPlaytimeByDay(anyString(), eq(responseString))).thenReturn(responseString);
+
+        mockMvc.perform(post("/api/session/history")
+                        .contentType(MediaType.TEXT_PLAIN)
+                        .content(responseString))
+                .andExpect(status().isOk())
+                .andExpect(result -> responseString.equals(result));
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void startCustomSessionTest() throws Exception {
+        SessionDTO sessionDTO = new SessionDTO();
+        sessionDTO.setName("test");
+        GameSession gameSession = new GameSession();
+        gameSession.setName("test");
+
+        when(gameSessionService.customStartGame(eq(sessionDTO), anyString())).thenReturn(gameSession);
+
+        mockMvc.perform(post("/api/session/custom-start")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(sessionDTO)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void getEarliestSessionTest() throws Exception {
+        GameSession gameSession = new GameSession();
+        gameSession.setName("early session");
+
+        when(queriesService.getEarliestSession()).thenReturn(gameSession);
+
+        mockMvc.perform(get("/api/session/earliest"))
+                .andExpect(status().isOk())
+                .andExpect(result -> result.equals(gameSession));
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void getListFromSqlQuery() throws Exception {
+        String query = "SQL query";
+        GameSession gameSession = new GameSession();
+        gameSession.setName("session");
+        List<GameSession> gameSessions = new ArrayList<>();
+        gameSessions.add(gameSession);
+
+        when(filterService.getListFromFilterQuery(query)).thenReturn(gameSessions);
+
+        mockMvc.perform(get("/api/session/custom")
+                        .param("query", query))
+                .andExpect(status().isOk())
+                .andExpect(result -> result.equals(gameSession));
+    }
 }
